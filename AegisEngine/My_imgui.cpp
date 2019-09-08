40965725f60f5ab0368760f06d3223c575726eb0
菅野翔吾 <ksyougo0918@gmail.com>
2019-09-08 17:57:09 +0900

    Imguiの修正

--- AegisEngine/My_imgui.cpp
+++ AegisEngine/My_imgui.cpp
@@ -128,6 +128,47 @@ void My_imgui::Draw(void)
 		}
 
 		{
+			if (ImGui::BeginMainMenuBar())
+			{
+				if (ImGui::BeginMenu("File"))
+				{
+					if (ImGui::MenuItem("New")) {}
+					if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+					if (ImGui::BeginMenu("Open Recent"))
+					{
+						ImGui::MenuItem("fish_hat.c");
+						ImGui::MenuItem("fish_hat.inl");
+						ImGui::MenuItem("fish_hat.h");
+						if (ImGui::BeginMenu("More.."))
+						{
+							ImGui::MenuItem("Hello");
+							ImGui::MenuItem("Sailor");
+							if (ImGui::BeginMenu("Recurse.."))
+							{
+								
+								ImGui::EndMenu();
+							}
+							ImGui::EndMenu();
+						}
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Edit"))
+				{
+					if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+					if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+					ImGui::Separator();
+					if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+					if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+					if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+					ImGui::EndMenu();
+				}
+				ImGui::EndMainMenuBar();
+			}
+		}
+
+		{
 			string name("garage");
 
 			//PLAYER* player = SCENE::Get_Game_Object<PLAYER>();
