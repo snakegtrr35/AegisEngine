5fcea050c5fbd361eb445362f05422b1d5e750e3
菅野翔吾 <ksyougo0918@gmail.com>
2020-01-30 14:57:16 +0900

    修正

--- AegisEngine/Light_hlsl.h
+++ AegisEngine/Light_hlsl.h
@@ -1,50 +1,50 @@
 #ifndef LIGHT_HLSL_H
 #define LIGHT_HLSL_H
 
-// マトリクスバッファ
-struct Lights
-{
-    // 共通部分
-    bool        Enable          /*: packoffset(c0)*/;
-    float3      Position        /*: packoffset(c0.x)*/;
-    float4      Color           /*: packoffset(c1)*/;
-    float       Radius          /*: packoffset(c2.x)*/;
-    float3      Attenuation     /*: packoffset(c2.y)*/;
-};
-
-// マトリクスバッファ
-cbuffer LightsBuffer : register(b10)
-{
-    Lights LightsBuf;
-}
-
-float3 DoPointLight(Lights light, float4 Position, float4 CameraPos, float4 Normal)
-{
-    float3 result = (float)1.0;
-
-    float3 Dir = light.Position - Position.xyz;
-    //float3 Dir = Position.xyz - light.Position;
-    float distance = length(Dir);
-    Dir = normalize(Dir);
-
-    //拡散
-    float colD = 0.5 - 0.5 * dot(Normal.xyz, Dir);
-
-    float colA = saturate(1.0f / (light.Attenuation.x + light.Attenuation.y * distance + light.Attenuation.z * distance * distance));
-
-    result = light.Color.rgb * (colD * colA);
-    return result;
-}
-
-bool Cul_Radius(Lights light, float4 Position)
-{
-    float x = (Position.xyz.x - light.Position.x) * (Position.xyz.x - light.Position.x);
-    float y = (Position.xyz.y - light.Position.y) * (Position.xyz.y - light.Position.y);
-    float z = (Position.xyz.z - light.Position.z) * (Position.xyz.z - light.Position.z);
-
-    float Dir = x + y + z;
-
-    return (Dir <= (light.Radius * light.Radius));
-}
+//// ライトバッファ
+//struct Lights
+//{
+//    // 共通部分
+//    bool        Enable          /*: packoffset(c0)*/;
+//    float3      Position        /*: packoffset(c0.x)*/;
+//    float4      Color           /*: packoffset(c1)*/;
+//    float       Radius          /*: packoffset(c2.x)*/;
+//    float3      Attenuation     /*: packoffset(c2.y)*/;
+//};
+//
+//// マトリクスバッファ
+//cbuffer LightsBuffer : register(b10)
+//{
+//    Lights LightsBuf;
+//}
+//
+//float3 DoPointLight(Lights light, float4 Position, float4 CameraPos, float4 Normal)
+//{
+//    float3 result = (float)1.0;
+//
+//    float3 Dir = light.Position - Position.xyz;
+//    //float3 Dir = Position.xyz - light.Position;
+//    float distance = length(Dir);
+//    Dir = normalize(Dir);
+//
+//    //拡散
+//    float colD = 0.5 - 0.5 * dot(Normal.xyz, Dir);
+//
+//    float colA = saturate(1.0f / (light.Attenuation.x + light.Attenuation.y * distance + light.Attenuation.z * distance * distance));
+//
+//    result = light.Color.rgb * (colD * colA);
+//    return result;
+//}
+//
+//bool Cul_Radius(Lights light, float4 Position)
+//{
+//    float x = (Position.xyz.x - light.Position.x) * (Position.xyz.x - light.Position.x);
+//    float y = (Position.xyz.y - light.Position.y) * (Position.xyz.y - light.Position.y);
+//    float z = (Position.xyz.z - light.Position.z) * (Position.xyz.z - light.Position.z);
+//
+//    float Dir = x + y + z;
+//
+//    return (Dir <= (light.Radius * light.Radius));
+//}
 
 #endif // !LIGHT_HLSL_H
--- AegisEngine/pixelShader.hlsl
+++ AegisEngine/pixelShader.hlsl
@@ -7,50 +7,51 @@
 
 
 
-//// マトリクスバッファ
-//struct Lights
-//{
-//	// 共通部分
-//    bool        Enable          /*: packoffset(c0)*/;
-//    float3      Position        /*: packoffset(c0.x)*/;
-//    float4      Color           /*: packoffset(c1)*/;
-//    float       Radius          /*: packoffset(c2.x)*/;
-//    float3      Attenuation     /*: packoffset(c2.y)*/;
-//};.
+// ライトバッファ
+struct Lights
+{
+    // 共通部分
+    bool Enable /*: packoffset(c0)*/;
+    float3 Position /*: packoffset(c0.x)*/;
+    float4 Color /*: packoffset(c1)*/;
+    float Radius /*: packoffset(c2.x)*/;
+    float3 Attenuation /*: packoffset(c2.y)*/;
+};
 
-//// マトリクスバッファ
-//cbuffer LightsBuffer : register(b10)
-//{
-//    Lights LightsBuf;
-//}
+// マトリクスバッファ
+cbuffer LightsBuffer : register(b10)
+{
+    Lights LightsBuf;
+}
 
-//float3 DoPointLight(Lights light, float4 Position, float4 CameraPos, float4 Normal)
-//{
-//    float3 result = (float) 1.0;
+float3 DoPointLight(Lights light, float4 Position, float4 CameraPos, float4 Normal)
+{
+    float3 result = (float) 1.0;
 
-//    float3 Dir = light.Position - Position.xyz;
-//    float distance = length(Dir);
-//    Dir = normalize(Dir);
+    float3 Dir = light.Position - Position.xyz;
+    //float3 Dir = Position.xyz - light.Position;
+    float distance = length(Dir);
+    Dir = normalize(Dir);
 
-//	//拡散
-//    float colD = 0.5 - 0.5 * dot(Normal.xyz, Dir);
+    //拡散
+    float colD = 0.5 - 0.5 * dot(Normal.xyz, Dir);
 
-//    float colA = saturate(1.0f / (light.Attenuation.x + light.Attenuation.y * distance + light.Attenuation.z * distance * distance));
+    float colA = saturate(1.0f / (light.Attenuation.x + light.Attenuation.y * distance + light.Attenuation.z * distance * distance));
 
-//    result = light.Color.rgb * (colD * colA);
-//    return result;
-//}
+    result = light.Color.rgb * (colD * colA);
+    return result;
+}
 
-//bool Cul_Radius(Lights light, float4 Position)
-//{
-//    float x = (Position.xyz.x - light.Position.x) * (Position.xyz.x - light.Position.x);
-//    float y = (Position.xyz.y - light.Position.y) * (Position.xyz.y - light.Position.y);
-//    float z = (Position.xyz.z - light.Position.z) * (Position.xyz.z - light.Position.z);
-    
-//    float Dir = x + y + z;
-    
-//    return (Dir <= (light.Radius * light.Radius));
-//}
+bool Cul_Radius(Lights light, float4 Position)
+{
+    float x = (Position.xyz.x - light.Position.x) * (Position.xyz.x - light.Position.x);
+    float y = (Position.xyz.y - light.Position.y) * (Position.xyz.y - light.Position.y);
+    float z = (Position.xyz.z - light.Position.z) * (Position.xyz.z - light.Position.z);
+
+    float Dir = x + y + z;
+
+    return (Dir <= (light.Radius * light.Radius));
+}
 
 
 
@@ -200,7 +201,7 @@ void main( PS_IN Input,
     //if (1 == LightsBuf.Enable)
     if (LightsBuf.Enable)
     {
-        //if (Cul_Radius(LightsBuf, Input.WPos))
+        if (Cul_Radius(LightsBuf, Input.WPos))
         {
             light_color = DoPointLight(LightsBuf, Input.WPos, CameraPos, Input.Normal);
             light_color = saturate(light_color);
