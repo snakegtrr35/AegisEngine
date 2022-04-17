2d2358ef106b5c89072ef619c146bb55bb439625
snakegtrr35 <44777882+snakegtrr35@users.noreply.github.com>
2022-04-18 01:20:44 +0900

    使用していないフォントファイルを削除

---
+++ AegisEngine/include/engine/core/hash/hash.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#ifndef _INCLEDE_HASH_H_
+#define _INCLEDE_HASH_H_
+
+namespace aegis
+{
+	// !文字列からハッシュ値を作成して取得
+	uint32 HashCRC32(const std::string_view& str) noexcept;
+}
+
+#endif // !_INCLEDE_HASH_H_
