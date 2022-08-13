5b39c9b8806ce33b613382b46fa5ee8f384aeb50
snakegtrr35 <44777882+snakegtrr35@users.noreply.github.com>
2022-08-13 19:26:43 +0900

    float型を直接使用していたのを修正

--- AegisEngine/include/engine/core/math/MathDef.h
+++ AegisEngine/include/engine/core/math/MathDef.h
@@ -10,23 +10,25 @@ namespace aegis
 {
 	namespace Math
 	{
-		static constexpr float NaN = std::numeric_limits<float>::quiet_NaN();
-		static constexpr float Inf = std::numeric_limits<float>::infinity();
-		static constexpr float Epsilon = std::numeric_limits<float>::epsilon();
-		static constexpr float PI = 3.1415926535897931f;
-		static constexpr float PI2 = PI * 2.0f;
-		static constexpr float INV_PI = 1.0f / PI;
-		static constexpr float PI_DIV2 = PI * 0.5f;
+		static constexpr float32 NaN = std::numeric_limits<float>::quiet_NaN();
+		static constexpr float32 Inf = std::numeric_limits<float>::infinity();
+		static constexpr float32 Epsilon = std::numeric_limits<float>::epsilon();
+		static constexpr float32 PI = 3.1415926535897931f;
+		static constexpr float32 PI2 = PI * 2.0f;
+		static constexpr float32 INV_PI = 1.0f / PI;
+		static constexpr float32 PI_DIV2 = PI * 0.5f;
 
-		inline constexpr float DegreeToRadian(float degree) { return degree * (PI / 180.0f); }
-		inline constexpr float RadianToDegree(float radian) { return radian * (180.0f / PI); }
+		static constexpr float32 Float32Max = 3.402823466e+38f;
 
-		inline constexpr float Lerp(const float a, const float b, float t)
+		inline constexpr float32 DegreeToRadian(float degree) { return degree * (PI / 180.0f); }
+		inline constexpr float32 RadianToDegree(float radian) { return radian * (180.0f / PI); }
+
+		inline constexpr float32 Lerp(const float a, const float b, float t)
 		{
 			return a + t * (b - a);
 		}
 
-		inline constexpr double Lerp(const double a, const double b, double t)
+		inline constexpr float64 Lerp(const float64 a, const float64 b, float64 t)
 		{
 			return a + t * (b - a);
 		}
