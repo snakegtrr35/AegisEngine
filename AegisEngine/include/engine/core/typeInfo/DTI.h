7abe86bfb4c2370dcb005eda02fc0ebc19586df4
snakegtrr35 <44777882+snakegtrr35@users.noreply.github.com>
2022-08-13 19:19:13 +0900

    説明コメントの追加 アサートが意図した動作をしないのを修正

--- AegisEngine/include/engine/core/typeInfo/DTI.h
+++ AegisEngine/include/engine/core/typeInfo/DTI.h
@@ -2,23 +2,30 @@
 
 #include "typeInfo.h"
 
-
-#define OBJECT_TYPE_INFO(_class_)\
+// 下記以外のAegisObjectの継承クラス
+#define OBJECT_TYPE_INFO(_base_class_, _class_)\
 public:\
+    using Base = _base_class_;\
     static aegis::typeInfo::TypeInfo* getDTI();\
     aegis::uint32 virtual GetTypeId();\
 
-#define ABSTRACT_OBJECT_TYPE_INFO(_class_)\
-    OBJECT_TYPE_INFO(_class_)\
+// 抽象クラスのDTI
+#define ABSTRACT_OBJECT_TYPE_INFO(_base_class_, _class_)\
+    OBJECT_TYPE_INFO(_base_class_, _class_)\
 
+// 全ての基底クラスのDTI
 #define BASE_OBJECT_TYPE_INFO(_class_)\
 public:\
     static aegis::typeInfo::TypeInfo* getDTI();\
 
 
 
+//static_assert(std::is_base_of<_base_class_, _class_>::value); \
+
+// OBJECT_TYPE_INFO の実体
 #define IMPLEMENT_OBJECT_TYPE_INFO(_base_class_, _class_)\
-    static_assert(std::is_same<_base_class_, _class_>::value == false);\
+    static_assert(std::is_base_of<_base_class_, _class_>::value\
+                && std::is_convertible<_class_*, _base_class_*>::value);\
     using namespace aegis::typeInfo;\
     aegis::typeInfo::TypeInfo* _class_::getDTI()\
     {\
@@ -33,7 +40,7 @@ public:\
     
     //aegis::typeInfo::TypeInfo _class_::DTI(_base_class_::getDTI(), #_class_, new IClassCreater<_class_>());\
 
-
+// BASE_OBJECT_TYPE_INFO の実体
 #define IMPLEMENT_BASE_OBJECT_TYPE_INFO(_class_)\
     using namespace aegis::typeInfo;\
     aegis::typeInfo::TypeInfo* _class_::getDTI()\
@@ -45,6 +52,7 @@ public:\
     //aegis::typeInfo::TypeInfo _class_::DTI(nullptr, #_class_, nullptr, true);\
 
 
+// ABSTRACT_OBJECT_TYPE_INFO の実体
 #define IMPLEMENT_ABSTRACT_OBJECT_TYPE_INFO(_base_class_, _class_)\
     using namespace aegis::typeInfo;\
     aegis::typeInfo::TypeInfo* _class_::getDTI()\
